ee748b49aa0ebdaa8fbf728c9fa3198f472dc363
0xukhezo <ukhezo.web3@gmail.com>
2022-09-27 13:16:58 +0200

    Final setup for Hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("hardhat-deploy")
+require("solidity-coverage")
+
+require("dotenv").config()
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.9",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
+}
