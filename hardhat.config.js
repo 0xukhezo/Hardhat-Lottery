498a32d48542ff561bbed09364ca22a945eac3ad
0xukhezo <ukhezo.web3@gmail.com>
2022-10-11 12:22:03 +0200

    Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,12 @@ require("solidity-coverage")
 
 require("dotenv").config()
 
+const GOERLI_URL_RPC = process.env.URL_RPC_GOERLI
+const LOCALHOST_URL_RPC = process.env.URL_RPC_LOCALHOST
+const PRIVATE_KEY_ACCOUNT_GOERLI = process.env.PRIVATE_KEY_GOERLI
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: {
@@ -17,5 +23,25 @@ module.exports = {
                 version: "0.6.6",
             },
         ],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_URL_RPC,
+            accounts: [PRIVATE_KEY_ACCOUNT_GOERLI],
+        },
     },
 }
