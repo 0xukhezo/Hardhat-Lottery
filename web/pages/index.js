9c4d07d0b52283f5f24092e9f9eca1d860968716
0xukhezo <ukhezo.web3@gmail.com>
2022-10-18 17:06:02 +0200

    Change manual connect button to web3uikit

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,16 +1,16 @@
-import Head from "next/head";
-import Navbar from "../components/Navbar/Navbar";
-import styles from "../styles/Home.module.css";
+import Head from "next/head"
+import Navbar from "../components/Navbar/Navbar"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Smart Contract Lottery</title>
-        <meta name="description" content="Smart Contract Lottery" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Smart Contract Lottery</title>
+                <meta name="description" content="Smart Contract Lottery" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar />
+        </div>
+    )
 }
